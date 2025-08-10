1c42195220ebc734809b9e193e11b1d6c803b3e3
Turt3l <mikelisekmanis13@gmail.com>
2025-08-10 20:53:12 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function FloatingConfetti() {
         const size = Math.random() * 24 + 8;
         const shapeTypes = ["circle"];
         const shape = shapeTypes[Math.floor(Math.random() * shapeTypes.length)];
-        const colors = ["pink"];
+        const colors = ["red"];
         const color = colors[Math.floor(Math.random() * colors.length)];
 
         let clipPath = "";
@@ -88,35 +88,41 @@ function App() {
     setShowButtons(false);
     setStage(1);
     setAnimationSequence([
-      "Everything you do, everything you say",
+      "Today is the first day of my trip",
       3000,
-      "Every move you take, every thought you express",
+      "And I miss you a lot",
       3000,
-      "Is absolutely perfect, and nothing will convince me otherwise",
+      "I can only imagine us going on trips, on long beautiful trips",
       3000,
-      "I notice absolutely everything you do, and I can only be happy",
+      "Where we go far, see many beautiful things and enjoy each others presence",
       3000,
-      "That such perfectness loves me, adores me, and is my soulmate for life",
+      "Where we make these stops in the gas station",
       3000,
-      "It brings ease into my life",
+      "And we get each other things we want, drinks, snacks",
       3000,
-      "Knowing that I have you, that I can trust you",
+      "But in the end, we like each others snack more, so we switch",
       3000,
-      "And that you trust me to provide you, to protect you and be by your side",
+      "We kiss, I hold my hand on your thigh while I drive",
       3000,
-      "I am absolutely grateful to have you",
+      "We look at each other, we laugh",
       3000,
-      "And I cannot wait for the future with you",
+      "You turn on the music, we listen to it and sing to it together",
       3000,
-      "A future where we live together, share things together",
+      "Life will be complete with you next to me",
       3000,
-      "Where we go on roadtrips together and make our favourite memories together",
+      "And you are my meaning to life, you are the reason to live",
       3000,
-      "Its a life worth living, the only life worth living is a life with you",
+      "Knowing that you are on this planet makes it so much easier",
       3000,
-      "You and only you",
+      "Knowing that you are all mine, that we will go on adventures together",
       3000,
-      "I love you so much <3",
+      "Makes every day pass away more easily",
+      3000,
+      "Because I know that every day is closer to use finally living together",
+      3000,
+      "I miss you my love",
+      3000,
+      "More than anything <3",
     ]);
   };
 
@@ -124,33 +130,168 @@ function App() {
     setShowButtons(false);
     setStage(1);
     setAnimationSequence([
-      "Everything about you is so loveable, so cherishable",
+      "Today I have arrived to Germany, I am in Wolfsburg",
       3000,
-      "I want to love every single part of you forever",
+      "The land of volkswagen sausages (check https://en.wikipedia.org/wiki/Volkswagen_currywurst for more info)",
       3000,
-      "If it means not loving myself, I dont care",
+      "I would want to travel all around the world with you",
       3000,
-      "You are a part of my soul, you are a part of me",
+      "Having the joy of reaching our destination, being able to get out",
       3000,
-      "A part which cannot be taken away",
+      "And breathe the fresh air",
       3000,
-      "A part which I will never forget and will cherish forever",
+      "Look in each others eyes, and see them filled with happiness",
       3000,
-      "I love your beautiful hair",
+      "Reaching the destination was no more exciting than getting there",
       3000,
-      "I love your beautiful smile",
+      "Because every moment with you is exciting and beautiful",
       3000,
-      "I love your beautiful personality and mind",
+      "No matter how boring it may seem",
       3000,
-      "I am so happy to have you just for that, for what you are, yourself",
+      "I am always interested in everything you do, I find peace with you",
       3000,
-      "You are so valuable to me, and always will be",
+      "I could look at you for days on end and I would be intrigued",
       3000,
-      "May our love grow together, may we love each other more and more every day",
+      "Because that mind of yours is such a mystery",
       3000,
-      "You are the best wife ever and I never wish to loose you",
+      "Whats going on in there?",
       3000,
-      "Thank you for being just the way you are <3",
+      "Did I forgot to get her a snack, or is she just tired?",
+      3000,
+      "Turns out she is just on her last straw, not even putting matches in her eyes could make her stay woken up",
+      3000,
+      "So you sleep in the car, while I drive around the city, trying to find a parking lot",
+      3000,
+      "I park the car, give you my hoodie as a blanket and watch you fall asleep",
+      3000,
+      "And I fall asleep next to you, while looking at you and smiling",
+      3000,
+      "I am incredibly happy to have you, happy like never before",
+      3000,
+    ]);
+  };
+
+  const handleDayThree = () => {
+    setShowButtons(false);
+    setStage(1);
+    setAnimationSequence([
+      "Today I will be exploring around the city",
+      3000,
+      "Going on a tour of the factory",
+      3000,
+      "Visiting the famous Volkswagen car tower",
+      3000,
+      "And just walking around the city",
+      3000,
+      "When we go somewhere",
+      3000,
+      "We will explore everything, the old town, museums",
+      3000,
+      "We will do many fun things together",
+      3000,
+      "Get ice cream, explore the city and do fun activities together",
+      3000,
+      "Everything with you is fun",
+      3000,
+      "Just sitting next to you doing nothing feels like I am doing something",
+      3000,
+      "Because it feels fun, it feels right and never does it feel boring to be with you",
+      3000,
+      "You are absolutely amazing and I love you so much",
+      3000,
+      "I cannot wait to play cards with you in the most random spots",
+      3000,
+      "To stop alongside a highway to pull out chairs and eat sandwhiches together",
+      3000,
+      "To me pushing the car to the gas station while you sit inside and steer",
+      3000,
+      "All of those moments would be the best moments of my life",
+      3000,
+      "Only because you are in them <3",
+    ]);
+  };
+
+  const handleDayFour = () => {
+    setShowButtons(false);
+    setStage(1);
+    setAnimationSequence([
+      "Today we will be traveling to another city",
+      3000,
+      "During these days I have been starting to miss you a lot",
+      3000,
+      "I would give anything to be with you right now, to be next to you",
+      3000,
+      "To feel your warmth and touch",
+      3000,
+      "I miss going to the gas station together",
+      3000,
+      "I miss eating ice cream together",
+      3000,
+      "I miss going for late night walks",
+      3000,
+      "Oh god I miss all these things so much",
+      3000,
+      "It just makes me want to cry",
+      3000,
+      "Why did it have to be over so quickly",
+      3000,
+      "The only thing thats holding me together is knowing that one day we will be together again",
+      3000,
+      "In each others arms, in comfort",
+      3000,
+      "Looking at each other",
+      3000,
+      "With such trust, care",
+      3000,
+      "That nothing in this world can break us up",
+      3000,
+      "We are indestructable",
+      3000,
+      "We will make it through everything",
+      3000,
+      "I will always be by your side",
+      3000,
+      "Forever.",
+    ]);
+  };
+
+  const handleDayFive = () => {
+    setShowButtons(false);
+    setStage(1);
+    setAnimationSequence([
+      "Today I am coming back home",
+      3000,
+      "I am coming back home to my most beautiful, most prettiest wife ever",
+      3000,
+      "During this whole trip there was never a time when I didnt think about you",
+      3000,
+      "There was never a moment when I didnt imagine us going on a trip together",
+      3000,
+      "Spending time together, looking at each other",
+      3000,
+      "Its beautiful, it really is",
+      3000,
+      "The bond that we have I will never find in anyone else",
+      3000,
+      "The understanding, the fiestiness",
+      3000,
+      "You are unique, you are the best, you are perfect",
+      3000,
+      "You are my wife",
+      3000,
+      "And I want to spend my whole life with you, no matter how much time it takes",
+      3000,
+      "Today I am coming home to you, finally",
+      3000,
+      "This weekend is ours, lets spend it together",
+      3000,
+      "Give me a call when I come home, lets play some minecraft?",
+      3000,
+      "Lets spend some time together, time that we need so desperately <3",
+      3000,
+      "I missed you",
+      3000,
+      "I really did <3",
     ]);
   };
 
@@ -158,10 +299,19 @@ function App() {
     showButtons ? (
       <>
         <button onClick={handleYourAttentionToDetail} style={buttonStyle}>
-          You are perfect the way you are
+          Day 1
         </button>
         <button onClick={handleILoveEverythingAboutYou} style={buttonStyle}>
-          I love everything about you
+          Day 2
+        </button>
+        <button onClick={handleDayThree} style={buttonStyle}>
+          Day 3
+        </button>
+        <button onClick={handleDayFour} style={buttonStyle}>
+          Day 4
+        </button>
+        <button onClick={handleDayFive} style={buttonStyle}>
+          Day 5
         </button>
       </>
     ) : null;
@@ -209,29 +359,19 @@ function App() {
               {!animationSequence && !showButtons && (
                 <TypeAnimation
                   sequence={[
-                    "You make me so incredibly happy",
+                    "I will miss you so much these days",
                     3000,
-                    "Never in eternity do I wish to lose you",
+                    "And I want you to look at each one of these every day",
                     3000,
-                    "You are what makes eternity sound like a short ammount of time",
+                    "You are the most important thing I have",
                     3000,
-                    "Because time flies by with you, it really does",
+                    "I cannot wait to go on our first roadtrip <3",
                     3000,
-                    "Every second of my life that I spend with you is the best time of my life",
+                    "Together <3",
                     3000,
-                    "Nothing is better than the calm, peaceful, fun, loving time spent with you",
+                    "I love you so much wifey",
                     3000,
-                    "You make life worth living, every second of it",
-                    3000,
-                    "Every smile you give feels like a dream",
-                    3000,
-                    "Every 'I love you' means so much to me",
-                    3000,
-                    "Like I would never hear those words from anyone else",
-                    3000,
-                    "And I never want to hear them from anyone else",
-                    3000,
-                    "Other than you <3",
+                    "And I already miss you, so so much",
                     3000,
                     () => setShowButtons(true),
                   ]}
@@ -269,8 +409,8 @@ function App() {
         </header>
         {showMessage && (
           <div className="quote">
-            “Love is that condition in which the happiness of another person is
-            essential to your own.”
+            “I would rather spend one lifetime with you, than face all the ages
+            of this world alone.”
           </div>
         )}
       </div>
@@ -304,7 +444,7 @@ const buttonStyle = {
   borderRadius: "5px",
   border: "none",
   background: "white",
-  color: "purple",
+  color: "pink",
   transition: "transform 0.3s, box-shadow 0.3s",
   boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
 };
